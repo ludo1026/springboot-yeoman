2c1ef04f89b730fc2931b2ca746b845ed5ce6a00
Ludovic Chaboud <ludo1026@free.fr>
2015-05-27 15:36:10 +0200

    Create Application.java

---
+++ src/main/java/Application.java
@@ -0,0 +1,83 @@
+package demo;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.Principal;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.util.WebUtils;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class UiApplication extends WebMvcConfigurerAdapter {
+
+	@Value("${useGrunt}")
+	private boolean useGrunt;
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiApplication.class, args);
+	}
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		System.out.println("*******************************************");
+		System.out.println(useGrunt);
+		if(!useGrunt) {
+			return;
+		}
+
+		// yo: file:///Users/ludovicchaboud/Code/workspace_immo3/myimmo/ui/yo
+		File file = new File("yo");
+
+		String absolutePath;
+		if(file.getAbsolutePath().indexOf('/') == 0) {
+            absolutePath = "file://" + file.getAbsolutePath();
+        } else {
+            absolutePath = "file:///" + file.getAbsolutePath();
+        }
+
+		String client = absolutePath+"/client/";
+        String tmp = absolutePath+"/.tmp/";
+
+        registry.addResourceHandler("/**")
+                .addResourceLocations(client, tmp)
+                .setCachePeriod(0)
+                .resourceChain(false);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
+
+}
